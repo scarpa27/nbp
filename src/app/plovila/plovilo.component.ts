9e5ee70b6872be7071d6938f30379efeb29fbf80
Toni <50109298+scarpa27@users.noreply.github.com>
2022-02-11 07:18:45 +0100

    added unversioned

---
+++ src/app/plovila/plovilo.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Plovilo} from "./plovilo.model";
+import {BehaviorSubject} from "rxjs";
+import {PloviloService} from "./plovilo.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+    selector: 'app-plovilo',
+    templateUrl: './plovilo.component.html',
+    styleUrls: ['./plovilo.component.css']
+})
+export class PloviloComponent implements OnInit, OnDestroy {
+
+    plovilo: Plovilo = new Plovilo();
+    subscription: BehaviorSubject<Plovilo> = new BehaviorSubject<Plovilo>(this.plovilo);
+
+    constructor(private ploviloService: PloviloService, private route: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.subscription = this.ploviloService.getPloviloAPI(this.route.snapshot.params['id']);
+
+        this.subscription.subscribe(
+            r => {this.plovilo = r;
+                }
+        )
+    }
+
+    ngOnDestroy(): void {
+    }
+
+}
